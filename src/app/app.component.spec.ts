b4296946e3adf7bee9e27a13082147443abfa043
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2022-02-08 13:46:58 +0200

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'LinkSide-Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LinkSide-Frontend');
+    expect(app.title).toEqual('LinkSide');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LinkSide-Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LinkSide app is running!');
   });
 });
